7beee5178d95f7e8282d00b93c41a17f83fecc36
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-08-06 20:22:11 -0300

    Acessando os dados do banco com PHP

--- data.php
+++ data.php
@@ -1,15 +1,9 @@
 <?php
 
-$books = [
-  ['id' => 1, 'title' => 'O Senhor dos Anéis', 'author' => 'J.R.R. Tolkien', 'description' => 'Uma aventura épica na Terra Média.'],
-  ['id' => 2, 'title' => 'Dom Casmurro', 'author' => 'Machado de Assis', 'description' => 'A história de Bentinho e Capitu.'],
-  ['id' => 3, 'title' => '1984', 'author' => 'George Orwell', 'description' => 'Um romance distópico sobre vigilância e controle.'],
-  ['id' => 4, 'title' => 'O Pequeno Príncipe', 'author' => 'Antoine de Saint-Exupéry', 'description' => 'Uma fábula sobre amizade e amor.'],
-  ['id' => 5, 'title' => 'Harry Potter e a Pedra Filosofal', 'author' => 'J.K. Rowling', 'description' => 'O começo da jornada de Harry no mundo da magia.'],
-  ['id' => 6, 'title' => 'A Revolução dos Bichos', 'author' => 'George Orwell', 'description' => 'Uma sátira sobre o totalitarismo.'],
-  ['id' => 7, 'title' => 'Cem Anos de Solidão', 'author' => 'Gabriel García Márquez', 'description' => 'A saga da família Buendía em Macondo.'],
-  ['id' => 8, 'title' => 'A Metamorfose', 'author' => 'Franz Kafka', 'description' => 'A transformação de Gregor Samsa em inseto.'],
-  ['id' => 9, 'title' => 'O Hobbit', 'author' => 'J.R.R. Tolkien', 'description' => 'A aventura de Bilbo Bolseiro.']
-];
+$db = new PDO('sqlite:database.sqlite');
+
+$query = $db->query("select * from books");
+
+$books = $query->fetchAll();
 
 ?>
