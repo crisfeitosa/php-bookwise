48431e18c31f196bf3f458f9ffc53f496e9b3ef4
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-08-28 12:24:32 -0300

    Listando os Meus Livros

--- controllers/my-books.controller.php
+++ controllers/my-books.controller.php
@@ -1,3 +1,11 @@
 <?php
 
-view('my-books');
+  if (! auth()) {
+    header('Location: /');
+
+    exit();
+  }
+
+  $books = $database->query("select * from books where user_id = :id", Book::class, ['id' => auth()->id]);
+
+  view('my-books', compact('books'));
--- views/my-books.view.php
+++ views/my-books.view.php
@@ -1,7 +1,21 @@
-<h1>Meus livros</h1>
+<h1 class="mt-6 font-bold text-lg">Meus livros</h1>
 
 <div class="grid grid-cols-4 gap-4">
-  <div class="col-span-3 gap-4 grid">
+  <div class="col-span-3 flex flex-col gap-4">
+    <?php foreach($books as $book): ?>
+      <div class="p-2 rounded border-stone-800 border-2 bg-stone-900">
+        <div class="flex">
+          <div class="w-1/3"><?= $book->user_id ?></div>
+            <div class="space-y-1">
+              <a href="/book?id=<?= $book->id ?>" class="font-semibold hover:underline"><?= $book->title ?></a>
+              <div class="text-xs italic"><?= $book->author ?></div>
+              <div class="text-xs italic">⭐⭐⭐⭐⭐(3 Avaliações)</div>
+            </div>
+        </div>
+
+        <div class="text-sm mt-2"><?= $book->description ?></div>
+      </div>
+    <?php endforeach; ?>
   </div>
 
   <div>
